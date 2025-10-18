241c51248b60ea073781c6b42c41c3c91a34273f
YanisBenyoucef <benyoucefyanis.pro@gmail.com>
2025-10-18 23:58:52 +0200

    Ajout classe NomMachine

---
+++ src/main/java/fr/uvsq/cprog/collex/NomMachine.java
@@ -0,0 +1,29 @@
+package fr.uvsq.cprog.collex;
+
+public class NomMachine {
+
+    private String nom;
+    private String domaine;
+
+    public NomMachine(String n,String d) {
+
+        if (n.contains("."))
+            throw new IllegalArgumentException("Ne pas mettre de point dans un nom de machine");
+        if(!d.matches("^(?!-)([A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,}$")){
+            throw new IllegalArgumentException("Nom de domaine invalide");
+        }
+        this.nom=n;
+        this.domaine=d;
+        
+    }
+
+    public String getNom() {
+        return nom;
+    }
+        public String getDomaine() {
+        return domaine;
+    }
+    public String NomQualifié(){
+        return nom + "."+domaine;
+    }
+}
