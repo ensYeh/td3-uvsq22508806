e62a3ce1bc67394f4a473070486dc8662dab9476
YanisBenyoucef <benyoucefyanis.pro@gmail.com>
2025-10-18 22:58:27 +0200

    Ajout classe AdresseIP

---
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -0,0 +1,17 @@
+package fr.uvsq.cprog.collex;
+
+public class AdresseIP {
+    private String adresseIPV4;
+
+    public AdresseIP(String a) {
+        if (a.matches("^((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)$")) {
+            this.adresseIPV4 = a;
+        } else {
+            throw new IllegalArgumentException("Adresse IPv4 invalide : " + a);
+        }
+    }
+
+    public String getAdresseIPV4() {
+        return this.adresseIPV4;
+    }
+}
